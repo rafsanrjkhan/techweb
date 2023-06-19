7ab4b212311aba6d663b1da29b9572622f3b8e12
Rafsan Ananta <81804979+rafsan15-2745@users.noreply.github.com>
2023-06-20 00:10:23 +0600

    portal updates including home,products,about,contact,login,register,head,nav,topnav,footer

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'config.php'); ?>
+<ul>
+    
+    <li>Go to <a href="../techweb/portal/index.php">Portal</a></li>
+
+</ul>
---
+++ portal/partials/topnav.php
@@ -0,0 +1,9 @@
+<!-- top navbar -->
+    <div class="top-navbar " >
+         <p>WELCOME TO TECHWEB</p>
+    <div class="icons">
+        <a href="../login.php"><img src="../people/frontend/img/register.png" alt="" width="18px">Login</a>
+        <a href="../register.php"><img src="../people/frontend/img/register.png" alt="" width="18px">Register</a>
+         </div>
+    </div>
+<!-- top navbar -->
